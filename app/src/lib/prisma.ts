fa492a0bfd4325e60a21db14352583f64a061fd1
Rishi sippy <sippyrishu@gmail.com>
2026-01-15 20:37:26 +0530

    fixes

--- app/src/lib/prisma.ts
+++ app/src/lib/prisma.ts
@@ -4,11 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error']
-  })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ['error'] })
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
